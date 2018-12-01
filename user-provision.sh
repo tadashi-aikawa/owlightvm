47f180f06b0e67269ccf3853016e66db728826f0
tadashi-aikawa <syou.maman@gmail.com>
2018-12-01 12:53:21 +0900

    aliasの抜け漏れ修正

--- user-provision.sh
+++ user-provision.sh
@@ -29,16 +29,19 @@ alias cdg='cd $(gowl list | fzf)'
 
 alias ga='git add'
 alias gaa='git add --all'
+alias gc='git checkout $(git branch -l | grep -vE "^\*" | tr -d " " | fzf)'
+alias gcm='git commit -m'
+alias gcr='git branch -rl | grep -vE "HEAD|master" | tr -d " " | sed -r "s@origin/@@g" | fzf | xargs -i git checkout -b {} origin/{}'
+alias gcv='git commit -v'
 alias gd='git diff'
 alias gf='git fetch'
+alias gfx='for d in $(ls | fzf --multi); do cd $d && echo [$d] && git fetch --all --prune -q && git rev-list --count --left-right @{upstream}...HEAD | awk '{print " ↓"$1" ↑"$2}' && cd ..; done'
 alias gl='git log'
 alias gll='git log --oneline --all --graph --decorate'
 alias gls='git log -3'
 alias glll="git log --graph --date=format:'%Y-%m-%d %H:%M' --pretty=format:'%C(auto)%d%Creset %C(yellow reverse)%h%Creset %C(magenta)%ae%Creset %C(cyan)%ad%Creset%n%C(white bold)%w(80)%s%Creset%n%b'"
 alias gs='git status --short'
 alias gsv='git status -v'
-alias gcm='git commit -m'
-alias gcv='git commit -v'
 EOF
 
 # ssh
